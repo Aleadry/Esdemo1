625bff728ddf91ecc40e1aae3b05a292520156c3
“tangyu <1055209333@qq.com>
2019-04-18 15:24:20 +0800

    es

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let a="lj520";
 window.onload=function () {
     console.log(a);
-}
-const b = 123;
-console.log(b);
+};
+let [c,b="liji"]=["李杰",null]
+console.log(c+b);
+let [d,e]=["蓝色"]
+console.log(d+e);
+let [f,g="hong"]=["红色"]
+console.log(f+g)
